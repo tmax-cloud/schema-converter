0ec61644b8ea2b137e2a38f3972e2463df5b2030
SISILIA <soowh91@gmail.com>
2022-08-11 13:14:40 +0900

    indent refactoring

--- schema-converter/src/main/java/com/tmax/ck/main/Main.java
+++ schema-converter/src/main/java/com/tmax/ck/main/Main.java
@@ -32,7 +32,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class Main {
-//	static List<List<String>> keySheet = new ArrayList<List<String>>();
+	// static List<List<String>> keySheet = new ArrayList<List<String>>();
 	static Map<String, List<List<String>>> keySheetMap = new HashMap<String, List<List<String>>>();
 	static Integer sequence = new Integer(0);
 	static Map<String, JsonObject> schemaMap = new HashMap<String, JsonObject>();
@@ -50,10 +50,10 @@ public class Main {
 		System.out.println("Start");
 		try {
 			// schema 파일 혹은 CRD yaml 파일이 있는 root directory
-//			String rootDir = "C:\\schema\\";
+			// String rootDir = "C:\\schema\\";
 			String rootDir = "C:\\cicd-crd\\";
 			String tempDir = Long.toString(System.currentTimeMillis());
-			if (args.length%2 != 0) {
+			if (args.length % 2 != 0) {
 				System.out.println("=================================================");
 				System.out.println("ERROR: Wrong # of prameters");
 				System.out.println("=================================================");
@@ -61,21 +61,21 @@ public class Main {
 			}
 
 			for (String arg : args) {
-				switch(arg) {
-				case "root":
-					rootDir = args[Arrays.asList(args).indexOf(arg) + 1] + "/";
-					break;
-				case "output":
-					tempDir = args[Arrays.asList(args).indexOf(arg) + 1];
-					break;
-				case "translate":
-					autoTranslation = Boolean.parseBoolean(args[Arrays.asList(args).indexOf(arg) + 1]);
-					break;
+				switch (arg) {
+					case "root":
+						rootDir = args[Arrays.asList(args).indexOf(arg) + 1] + "/";
+						break;
+					case "output":
+						tempDir = args[Arrays.asList(args).indexOf(arg) + 1];
+						break;
+					case "translate":
+						autoTranslation = Boolean.parseBoolean(args[Arrays.asList(args).indexOf(arg) + 1]);
+						break;
 				}
 			}
 			String outputDir = rootDir + tempDir + "/";
 			System.out.println(autoTranslation);
-			
+
 			Translate translate = null;
 			if (autoTranslation) {
 				translate = TranslateOptions.newBuilder().build().getService();
@@ -130,14 +130,11 @@ public class Main {
 				// 하나의 yaml파일에 여러 CRD를 담고 있는 경우
 				// text를 parsing해서 (crdname.yaml, crd)로 yamlMap에 put
 				BufferedReader br = new BufferedReader(
-					new FileReader(yamlFile)
-				);
+						new FileReader(yamlFile));
 
 				BufferedWriter bw = new BufferedWriter(
-					new FileWriter(
-						new File(outputDir + "temp" + Integer.toString(cnt) + ".yaml")
-					)
-				);
+						new FileWriter(
+								new File(outputDir + "temp" + Integer.toString(cnt) + ".yaml")));
 
 				ArrayList plurals = new ArrayList();
 				Pattern pattern = Pattern.compile("^\\s{4}plural: [a-zA-Z]+$");
@@ -154,10 +151,8 @@ public class Main {
 						bw.flush();
 						bw.close();
 						bw = new BufferedWriter(
-							new FileWriter(
-								new File(outputDir + "temp" + Integer.toString(cnt) + ".yaml")
-							)
-						);
+								new FileWriter(
+										new File(outputDir + "temp" + Integer.toString(cnt) + ".yaml")));
 					}
 				}
 				bw.flush();
@@ -201,19 +196,16 @@ public class Main {
 
 			for (String yamlKey : yamlMap.keySet()) {
 				PrintWriter result = new PrintWriter(
-					new BufferedWriter(
-						new FileWriter(
-							new File(outputDir + yamlFiles[0].getName()), true
-						)
-					)
-				);
+						new BufferedWriter(
+								new FileWriter(
+										new File(outputDir + yamlFiles[0].getName()), true)));
 				mapper.writeValue(result, yamlMap.get(yamlKey));
 				// mapper.writeValue(new File(outputDir + yamlKey), yamlMap.get(yamlKey));
 			}
 
 			// batch 처리를 위해서 작성한 코드. 현재 payload가 너무 크면 400 에러가 발생하는 문제가 있어서 주석처리
-//			List<Translation> translatedList = translate.translate(originalList,
-//					TranslateOption.sourceLanguage("en").targetLanguage("ko"));
+			// List<Translation> translatedList = translate.translate(originalList,
+			// TranslateOption.sourceLanguage("en").targetLanguage("ko"));
 
 			// CSV에서 콤마가 separator로 인식됨, 이스케이프 처리 하기 싫어서 OOXML 이용
 			// 단일 파일에
@@ -279,7 +271,7 @@ public class Main {
 
 	// value가 Map의 자식클래스일 경우 재귀로 하위탐색, key가 description일 경우 코드 변환
 	static void convertCrdDescriptionToCode(List<List<String>> keySheet, Map<String, Object> yaml, String path) {
-		//System.out.println(yaml.keySet());
+		// System.out.println(yaml.keySet());
 		for (String key : yaml.keySet()) {
 			if (yaml.get(key) instanceof Map<?, ?>) {
 				convertCrdDescriptionToCode(keySheet, (Map<String, Object>) yaml.get(key), path + "." + key);
@@ -289,15 +281,16 @@ public class Main {
 			 * 구성요소들이 List<?> 일 경우 이슈 나오면 처리할 것임
 			 */
 			// value가 List 일 경우 예외처리(임시)
-			else if (yaml.get(key) instanceof List<?>) { 
+			else if (yaml.get(key) instanceof List<?>) {
 				for (Object obj : (List) yaml.get(key)) {
 					if (obj instanceof Map<?, ?>) {
 						convertCrdDescriptionToCode(keySheet, (Map<String, Object>) obj, path + "." + key);
 					}
 				}
-			} 
+			}
 			/*
-			 * jsonSchema 가 CRD yaml 아래에는 openAPIV3Schema 로 들어가는데, openAPIV3Schema 아닌 곳에도 description을 key로 가지는 오브젝트 다수 발견
+			 * jsonSchema 가 CRD yaml 아래에는 openAPIV3Schema 로 들어가는데, openAPIV3Schema 아닌 곳에도
+			 * description을 key로 가지는 오브젝트 다수 발견
 			 * 우선 스펙상 openAPIV3Schema 만 structural schema에 포함되기 때문에 한정 지음
 			 * 비효율적인 코딩이지만 openAPIV3Schema 객체의 yaml path를 확정지을 수 없기 때문에 풀스캔
 			 */
